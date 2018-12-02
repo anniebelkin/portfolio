577f35d69c5e3626968f6c343facbd7ed66ca32e
Annie <Anniebelkin97@gmail.com>
2018-12-02 19:13:13 +0200

    Add mixins for responsive design

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    hexrgba = require('postcss-hexrgba');
+    hexrgba = require('postcss-hexrgba'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, hexrgba ,autoprefixer({ browsers: ['> 0%'] }) ]) )
+        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba ,autoprefixer({ browsers: ['> 0%'] }) ]) )
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
